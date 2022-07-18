ca31c1916a5911ed298f8bd64199abdffacac5f5
Valentina-Divina <105946453+Valentina-Divina@users.noreply.github.com>
2022-07-18 18:52:33 +0300

    Create interactive animation

---
+++ VKontakte/UIHelpers/CustomUINavigationController.swift
@@ -0,0 +1,99 @@
+//
+//  CustomUINavigationController.swift
+//  VKontakte
+//
+//  Created by Valya on 18.07.2022.
+//
+
+
+import UIKit
+
+class CustomInteractiveTransition: UIPercentDrivenInteractiveTransition {
+    var hasStarted = false
+    var shouldFinish = false
+    
+    var viewController: UIViewController? { didSet {
+        let recognizer = UIScreenEdgePanGestureRecognizer(target: self,
+                                                          action: #selector(handleScreenEdgeGesture(_:)))
+        recognizer.edges = [.left]
+        viewController?.view.addGestureRecognizer(recognizer) }
+    }
+    
+    @objc func handleScreenEdgeGesture(_ recognizer: UIScreenEdgePanGestureRecognizer) {
+        
+        switch recognizer.state {
+            
+        case .began:
+            //Пользователь начал тянуть
+            hasStarted = true
+            viewController?.navigationController?.popViewController(animated: true)
+            
+        case .changed:
+            //Пользователь продолжил тянуть
+            
+            guard let width = recognizer.view?.bounds.width else {
+                hasStarted = false
+                cancel()
+                return
+            }
+            
+            let translation = recognizer.translation(in: recognizer.view)
+            
+            let relativeTranslation = translation.x / width
+            
+            let progress = max(0, min(1, relativeTranslation))
+            
+            update(progress)
+            
+            shouldFinish = progress > 0.2
+            
+        case .ended:
+            hasStarted = false
+            shouldFinish ? finish() : cancel()
+            
+        case .cancelled:
+            hasStarted = false
+            cancel()
+            
+        default:
+            break
+        }
+    }
+}
+
+class CustomNavigationController: UINavigationController, UINavigationControllerDelegate {
+    
+    let interactiveTransition = CustomInteractiveTransition()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.delegate = self
+    }
+    
+    func navigationController(_ navigationController: UINavigationController, animationControllerFor operation: UINavigationController.Operation,
+                              from fromVC: UIViewController,
+                              to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        if operation == .push {
+            self.interactiveTransition.viewController = toVC
+            return PushAnimator()
+        }
+        else if operation == .pop {
+            if navigationController.viewControllers.first != toVC {
+                self.interactiveTransition.viewController = toVC
+            }
+            return PopAnimator()
+        }
+        return nil
+    }
+    
+    
+    func navigationController(
+        _ navigationController: UINavigationController,
+        interactionControllerFor animationController: UIViewControllerAnimatedTransitioning
+    ) -> UIViewControllerInteractiveTransitioning? {
+        
+        return interactiveTransition.hasStarted ? interactiveTransition : nil
+        
+    }
+}
---
+++ VKontakte/UIHelpers/PopPushAnimators.swift
@@ -0,0 +1,108 @@
+//
+//  PopPushAnimators.swift
+//  VKontakte
+//
+//  Created by Valya on 18.07.2022.
+//
+
+import UIKit
+
+class PushAnimator: NSObject, UIViewControllerAnimatedTransitioning {
+    private let animationDuration: TimeInterval = 1
+    
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return animationDuration
+    }
+    
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        guard let source = transitionContext.viewController(forKey: .from),
+            let destination = transitionContext.viewController(forKey: .to) else { return }
+        
+        transitionContext.containerView.addSubview(destination.view)
+        
+        destination.view.transform = .identity
+        source.view.transform = .identity
+        
+
+        
+        let rotationDest = CGAffineTransform(rotationAngle: -(CGFloat.pi / 2))
+        destination.view.setAnchorPoint(CGPoint(x: 0, y: 0))
+        source.view.setAnchorPoint(CGPoint(x: 0, y: 0))
+        destination.view.transform = rotationDest
+        
+        UIView.animateKeyframes(withDuration: animationDuration, delay: 0, options: .calculationModePaced, animations: {
+            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.75, animations: {
+                let rotation = CGAffineTransform(rotationAngle: (CGFloat.pi / 2))
+                source.view.transform = rotation
+            })
+            UIView.addKeyframe(withRelativeStartTime: 0.6, relativeDuration: 0.4, animations: {
+                destination.view.transform = .identity
+            })
+        }) { finished in
+            if finished && !transitionContext.transitionWasCancelled {
+                source.view.transform = .identity
+            }
+            transitionContext.completeTransition(finished && !transitionContext.transitionWasCancelled)
+        }
+    }
+}
+
+class PopAnimator: NSObject, UIViewControllerAnimatedTransitioning {
+    private let animationDuration: TimeInterval = 1
+    
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return animationDuration
+    }
+    
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        guard let source = transitionContext.viewController(forKey: .from),
+            let destination = transitionContext.viewController(forKey: .to) else { return }
+        
+        transitionContext.containerView.addSubview(destination.view)
+        transitionContext.containerView.sendSubviewToBack(destination.view)
+        
+        source.view.frame = transitionContext.containerView.frame
+        destination.view.frame = transitionContext.containerView.frame
+        
+        let rotation = CGAffineTransform(rotationAngle: (CGFloat.pi / 2))
+        destination.view.transform = rotation
+        
+        UIView.animateKeyframes(withDuration: animationDuration, delay: 0, options: .calculationModePaced, animations: {
+            UIView.addKeyframe(withRelativeStartTime: 0.25, relativeDuration: 0.75, animations: {
+                destination.view.transform = .identity
+            })
+            UIView.addKeyframe(withRelativeStartTime: 0.4, relativeDuration: 0.4, animations: {
+                let rotation = CGAffineTransform(rotationAngle: -(CGFloat.pi / 2))
+                source.view.transform = rotation
+            })
+        }) { finished in
+            if finished && !transitionContext.transitionWasCancelled {
+                source.removeFromParent()
+            } else if transitionContext.transitionWasCancelled {
+                destination.view.transform = .identity
+            }
+            transitionContext.completeTransition(finished && !transitionContext.transitionWasCancelled)
+        }
+    }
+}
+
+extension UIView {
+    func setAnchorPoint(_ point: CGPoint) {
+        var newPoint = CGPoint(x: bounds.size.width * point.x, y: bounds.size.height * point.y)
+        var oldPoint = CGPoint(x: bounds.size.width * layer.anchorPoint.x, y: bounds.size.height * layer.anchorPoint.y);
+
+        newPoint = newPoint.applying(transform)
+        oldPoint = oldPoint.applying(transform)
+
+        var position = layer.position
+
+        position.x -= oldPoint.x
+        position.x += newPoint.x
+
+        position.y -= oldPoint.y
+        position.y += newPoint.y
+
+        layer.position = position
+        layer.anchorPoint = point
+    }
+}
