a62df17095ab743e31c9ebb2d39b97240b270ae6
Valentina-Divina <105946453+Valentina-Divina@users.noreply.github.com>
2022-11-08 15:12:09 +0300

    add operation for groups

---
+++ VKontakte/Network/Operations/GetGroupsOperation.swift
@@ -0,0 +1,56 @@
+//
+//  GetGroupsOperation.swift
+//  VKontakte
+//
+//  Created by Valya on 08.11.2022.
+//
+
+import Foundation
+import Alamofire
+
+class GetGroupsOperation : Operation {
+    
+    var service: Service
+    
+    var result: GroupResponse?
+    
+    override func main() {
+        let url = service.baseUrl + "/groups.get"
+        let parameters: Parameters = [
+            "access_token": service.token,
+            "v":"5.131",
+            "extended":"1"]
+        
+        AF.request(url, method: .get, parameters: parameters).responseJSON { response in
+            if let data = response.data {
+                let groups = try! JSONDecoder().decode(GroupResponse.self, from: data)
+                self.result = groups
+            }
+        }
+    }
+    
+    init(service: Service) {
+        self.service = service
+        super.init()
+    }
+    
+}
+
+
+class ConvertGroupsOperation: Operation {
+    
+    var dataToConvert: GroupResponse
+    var convertedResult: [GroupCollection]?
+    
+    override func main() {
+        let converted = dataToConvert.response?.items.map({ group in
+            GroupCollection(name: group.name, imageUrl: group.photo200, id: group.id)
+        }) ?? []
+        convertedResult = converted
+    }
+    
+    init(dataToConvert: GroupResponse ) {
+        self.dataToConvert = dataToConvert
+        super.init()
+    }
+}
